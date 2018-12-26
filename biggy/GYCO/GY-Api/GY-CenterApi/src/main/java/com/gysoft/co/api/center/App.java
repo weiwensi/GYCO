4374791000f9053b220de058a436e47099aa4860
wws11 <17637900215@163.com>
2018-12-26 16:16:28 +0800

    ddd

---
+++ biggy/GYCO/GY-Api/GY-CenterApi/src/main/java/com/gysoft/co/api/center/App.java
@@ -0,0 +1,13 @@
+package com.gysoft.co.api.center;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
